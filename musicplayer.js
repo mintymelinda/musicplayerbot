6b28fc24e9cfe4d14e6b9b68f5169b4652f26b4f
Melinda Kerrigan <melindakerrigan@gmail.com>
2024-10-27 03:26:25 -0400

    big ol' refactor hopefully fixed skipping winning songs and improved logic for scheduling predictions

--- musicplayer.js
+++ musicplayer.js
@@ -1,45 +1,67 @@
-let headers = {
+const headers = {
   'Client-ID': TWITCH_CLIENT_ID,
   'Authorization': `Bearer ${TWITCH_OAUTH_TOKEN}`,
   'Content-Type': 'application/json'
 }
 
-var live = false;
-var player;
-var session_id;
-var videos = {};
-var active_prediction_id;
-var player;
-var playing = false;
-var PREDICTION_TIME_WINDOW = 60;
-var queries = new Map();
-
-const SONG_REDEMPTION_PROMPT = 'Enter youtube id or song / artist name';
+const SONG_REDEMPTION_PROMPT = 'Enter youtube id or title + artist name';
 const SONG_REDEMPTION_TITLE = "Add a song request";
 const SONG_REDEMPTION_COST = 10;
+const MAX_ENTRIES_FOR_PREDICTION = 10;
+const PREDICTION_TIME_WINDOW = 60;
 
-class CustomRewards {
-  constructor(rewards) {
-    this.rewards = rewards;
+var allow_live = false;
+var playing = false;
+var prediction_active = false;
+var prediction_scheduled = false;
+
+var player;
+var session_id;
+var active_prediction_id;
+
+var videos = {};
+var queries = new Map();
+
+function getVideoList() {
+  return Object.values(videos);
+}
+
+function haveVideosToPlay() {
+  return getVideoList().length > 0;
+}
+
+function haveVideosForPrediction() {
+  return getVideoList().length >= 2;
+}
+
+
+class YoutubeData {
+  constructor(ytData) {
+    this.videoId = ytData.id;
+    this.title = ytData.snippet.title;
+    this.live = ytData.snippet.liveBroadcastContent === "none" ? false : true;
+    this.duration = this.live ? undefined : this.YTDurationToSeconds(ytData.contentDetails.duration);
+    this.viewCount = ytData.statistics.viewCount;
   }
 
-  async createChannelPointRedeems() {
-    await fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?' + new URLSearchParams({
-      broadcaster_id: BROADCASTER_USER_ID,
-      only_manegeable_rewards: true
-    }), { headers: headers }
-    ).then(r => r.json().then(async response => {
-      for (var redeem of response?.data) {
-        await fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?' + new URLSearchParams({
-          broadcaster_id: BROADCASTER_USER_ID,
-          id: redeem.id
-        }), { method: 'DELETE', headers: headers });
-      }
-    }));
+  YTDurationToSeconds(duration) {
+    var match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
 
-    for (var reward of this.rewards) {
-      await reward.createChannelPointRedeem();
-    }
+    match = match.slice(1).map(function (x) {
+      if (x != null) {
+        return x.replace(/\D/, '');
+      }
+    });
+
+    var hours = (parseInt(match[0]) || 0);
+    var minutes = (parseInt(match[1]) || 0);
+    var seconds = (parseInt(match[2]) || 0);
+
+    return hours * 3600 + minutes * 60 + seconds;
+  }
+
+  isValid() {
+    return this.viewCount > 1000 && (allow_live || !this.live);
   }
 }
 
@@ -70,79 +92,54 @@ class CustomReward {
   }
 }
 
-var custom_rewards = new CustomRewards([
+var custom_rewards = [
   new CustomReward(SONG_REDEMPTION_TITLE, SONG_REDEMPTION_COST, true, SONG_REDEMPTION_PROMPT, true,
     async (event) => {
-      var ytData = queries.get(event.user_input) ?? await searchYouTube(event.user_input);
-      queries.set(event.user_input, ytData);
-      for (var data of ytData) {
-        if (data.isValid()) {
-          var videoRequest = new VideoRequest(event, data);
-          Object.defineProperty(videos, videoRequest.id, {
-            enumerable: true,
-            configurable: true,
-            value: videoRequest
-          });
-          break;
-        }
+      var data = queries.get(event.user_input) ?? await searchYouTube(event.user_input);
+      queries.set(event.user_input, data);
+      if (data.isValid()) {
+        Object.defineProperty(videos, data.videoId, {
+          enumerable: true,
+          configurable: true,
+          value: data
+        });
       }
     }),
   new CustomReward('Skip Song', 5000, false, undefined, true,
     () => {
       if (playing) {
-        if (getVideoList().length > 0) {
+        if (prediction_active) {
+          patchPrediction(active_prediction_id, 'CANCELED');
+          prediction_active = false;
+          prediction_scheduled = false;
+        }
+
+        if (haveVideosToPlay()) {
           loadNextVideo();
         } else {
           player.stopVideo();
         }
-        if (active_prediction_id && active_prediction_id !== -1) {
-          patchPrediction(active_prediction_id, 'CANCELED', undefined);
-        }
-        active_prediction_id = undefined;
       }
     }
   )
-]);
+];
 
-class YoutubeData {
-  constructor(ytData) {
-    this.videoId = ytData.id;
-    this.title = ytData.snippet.title;
-    this.thumbnail = ytData.snippet.thumbnails.standard;
-    this.duration = ytData.snippet.liveBroadcastContent ? undefined : this.YTDurationToSeconds(ytData.contentDetails.duration);
-    this.licensedContent = ytData.contentDetails.licensedContent;
-    this.viewCount = ytData.statistics.viewCount;
-    this.liveBroadcastContent = ytData.snippet.liveBroadcastContent;
-    this.categoryId = ytData.snippet.categoryId;
-  }
+async function createChannelPointRedeems() {
+  await fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?' + new URLSearchParams({
+    broadcaster_id: BROADCASTER_USER_ID,
+    only_manegeable_rewards: true
+  }), { headers: headers }
+  ).then(r => r.json().then(async response => {
+    for (var redeem of response?.data) {
+      await fetch('https://api.twitch.tv/helix/channel_points/custom_rewards?' + new URLSearchParams({
+        broadcaster_id: BROADCASTER_USER_ID,
+        id: redeem.id
+      }), { method: 'DELETE', headers: headers });
+    }
+  }));
 
-  YTDurationToSeconds(duration) {
-    var match = duration.match(/PT(\d+H)?(\d+M)?(\d+S)?/);
-
-    match = match.slice(1).map(function (x) {
-      if (x != null) {
-        return x.replace(/\D/, '');
-      }
-    });
-
-    var hours = (parseInt(match[0]) || 0);
-    var minutes = (parseInt(match[1]) || 0);
-    var seconds = (parseInt(match[2]) || 0);
-
-    return hours * 3600 + minutes * 60 + seconds;
-  }
-
-  isValid() {
-    return this.viewCount > 1000 && this.liveBroadcastContent === "none";
-  }
-}
-
-class VideoRequest {
-  constructor(event, ytData) {
-    this.id = event.id;
-    this.rewardId = event.reward.id;
-    this.redeemedAt = event.redeemed_at;
-    this.ytData = ytData;
+  for (var reward of custom_rewards) {
+    await reward.createChannelPointRedeem();
   }
 }
 
@@ -161,7 +158,8 @@ async function onYouTubeIframeAPIReady() {
       'onStateChange': onPlayerStateChange
     }
   });
-  await custom_rewards.createChannelPointRedeems();
+
+  await createChannelPointRedeems();
   getWebSocket();
   update();
 }
@@ -183,34 +181,31 @@ async function subscribeToEvent(type, condition) {
 }
 
 async function searchYouTube(query) {
-  var results = [];
-
   var yt = await youtubeQuery(query);
   if (yt) {
-    results.push(yt);
-  } else {
-    var searchRegex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(?<videoId>.*?((?=[&#?])|$))/.exec(query)?.groups['videoId'];
-    if (searchRegex) {
-      results.push(await youtubeQuery(searchRegex));
-    } else {
-      for (var key of YOUTUBE_API_KEYS) {
-        var search = await fetch(`https://www.googleapis.com/youtube/v3/search?` + new URLSearchParams({
-          part: `snippet`,
-          q: query,
-          type: `video`,
-          key: key
-        })).then(r => r.json());
-        for (var r of search.items) {
-          results.push(await youtubeQuery(r.id.videoId));
-        }
-        if (search.items.length > 0) {
-          break;
-        }
-      }
-    }
+    return yt;
   }
 
-  return results;
+  var searchRegex = /(youtu.*be.*)\/(watch\?v=|embed\/|v|shorts|)(?<videoId>.*?((?=[&#?])|$))/.exec(query)?.groups['videoId'];
+  if (searchRegex) {
+    return await youtubeQuery(searchRegex);
+  } else {
+    var response;
+    for (var key of YOUTUBE_API_KEYS) {
+      var search = await fetch(`https://www.googleapis.com/youtube/v3/search?` + new URLSearchParams({
+        part: `snippet`,
+        q: query,
+        type: `video`,
+        key: key
+      })).then(r => r.json());
+
+      if (search?.items?.length > 0) {
+        response = await youtubeQuery(search.items[0].id.videoId);
+        break;
+      }
+    }
+    return response;
+  }
 }
 
 async function youtubeQuery(videoId) {
@@ -218,7 +213,7 @@ async function youtubeQuery(videoId) {
   for (var key of YOUTUBE_API_KEYS) {
     var q = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails,statistics&id=${videoId}&key=${key}`).then(r => r.json());
     if (q?.items?.length > 0) {
-      data = new YoutubeData(q.items[0])
+      data = new YoutubeData(q.items[0]);
       break;
     }
   }
@@ -228,7 +223,6 @@ async function youtubeQuery(videoId) {
 function onPlayerStateChange(event) {
   if (event.data === YT.PlayerState.ENDED || event.data === YT.PlayerState.STOPPED || event.data === YT.PlayerState.CUED) {
     playing = false;
-    live = false;
   }
 
   if (event.data === YT.PlayerState.PLAYING) {
@@ -236,29 +230,21 @@ function onPlayerStateChange(event) {
   }
 }
 
-function getVideoList() {
-  return Object.values(videos);
+function canStartPrediction() {
+  return !prediction_active && !prediction_scheduled && haveVideosForPrediction() && !getVideoList().find(x => x.playNext);
 }
 
-// this is the worse thing ever written
 function update() {
-  if (!live) {
-    if (!playing && getVideoList().length > 0 && (active_prediction_id === -1 || !active_prediction_id)) {
-      loadNextVideo();
-    } else if (!active_prediction_id && playing && getVideoList().length >= 2) {
+  if (playing) {
+    if (canStartPrediction()) {
       var time_remaining = player.getDuration() - player.getCurrentTime();
       if (time_remaining > PREDICTION_TIME_WINDOW) {
         waitForPrediction(time_remaining);
-      } else if (!live) {
-        for (var v of getVideoList()) {
-          time_remaining += v.duration;
-          var index = getVideoList().indexOf(v);
-          if (time_remaining > PREDICTION_TIME_WINDOW && getVideoList().length - index >= 2) {
-            waitForPrediction(time_remaining);
-            break;
-          }
-        }
       }
+    }
+  } else {
+    if (haveVideosToPlay()) {
+      loadNextVideo();
     }
   }
 
@@ -268,28 +254,30 @@ function update() {
 function loadNextVideo() {
   var video = getVideoList().find(x => x?.playNext) ?? getVideoList()[0];
 
-  console.log(`playNext: ${video.playNext} : ${video.ytData.videoId}`);
-  player.loadVideoById(video.ytData.videoId);
+  console.log(`playNext: ${video.playNext} : ${video.videoId}`);
+  player.loadVideoById(video.videoId);
   player.playVideo();
-  delete videos[video.id];
-  live = video.ytData.liveBroadcastContent === "none" ? false : true;
-  console.log(videos);
+  delete videos[video.videoId];
 }
 
 function waitForPrediction(duration) {
-  active_prediction_id = -1;
+  prediction_scheduled = true;
   var waitForPrediction = Math.round((duration - PREDICTION_TIME_WINDOW) * 1000);
   console.log(`prediction starting in: ${waitForPrediction / 1000}`);
   setTimeout(() => startPrediction(), waitForPrediction);
 }
 
 async function startPrediction() {
-  var pollEntries = getVideoList().sort(() => 0.5 - Math.random()).slice(0,10);
-  var outcomes = pollEntries.map(v => new Object({ title: v.ytData.title.substring(0, 25) }));
+  prediction_active = true;
+  prediction_scheduled = false;
+  var pollEntries = getVideoList().sort(() => 0.5 - Math.random()).slice(0, MAX_ENTRIES_FOR_PREDICTION);
+  var outcomes = pollEntries.map(v => new Object({ title: v.title.substring(0, 25) }));
 
+  // should never happen but ya-know
   if (outcomes.length < 2) {
-    active_prediction_id = undefined;
-    return 'not enough entries';
+    prediction_active = false;
+    console.log('not enough entries');
+    return;
   }
 
   fetch('https://api.twitch.tv/helix/predictions', {
@@ -311,23 +299,17 @@ async function startPrediction() {
           configurable: true,
           value: prediction.outcomes[i].id
         });
-
-        Object.defineProperty(pollEntries[i], 'prediction_id', {
-          writable: true,
-          configurable: true,
-          value: prediction.id
-        });
       }
     }
   })).catch((reason) => {
+    // outcomes contained one or more invalid words, or something else went wrong
     console.log(reason);
-    active_prediction_id = undefined;
+    prediction_active = false;
   });
 }
 
 function processPredictionLock(event) {
   if (event.id === active_prediction_id) {
-    active_prediction_id = undefined;
     var outcomes = event.outcomes;
     outcomes.sort((a, b) => {
       if (a.channel_points === b.channel_points)
@@ -346,6 +328,7 @@ function processPredictionLock(event) {
       configurable: true,
       value: true
     });
+    prediction_active = false;
   }
 }
 
@@ -368,10 +351,10 @@ async function getWebSocket() {
     switch (data.metadata.message_type) {
       case 'session_welcome':
         session_id = data.payload.session.id;
-        for (var redeem of custom_rewards.rewards) {
+        for (var reward of custom_rewards) {
           subscribeToEvent('channel.channel_points_custom_reward_redemption.add', {
             BROADCASTER_USER_ID: BROADCASTER_USER_ID,
-            reward_id: redeem.id
+            reward_id: reward.id
           });
         }
         subscribeToEvent('channel.prediction.lock', { BROADCASTER_USER_ID: BROADCASTER_USER_ID });
@@ -380,8 +363,8 @@ async function getWebSocket() {
         console.log(data);
         switch (data.payload.subscription.type) {
           case 'channel.channel_points_custom_reward_redemption.add':
-            var redeem = custom_rewards.rewards.find(x => x.id == data.payload.event.reward.id);
-            await redeem?.response(data.payload.event);
+            var reward = custom_rewards.find(x => x.id == data.payload.event.reward.id);
+            await reward?.response(data.payload.event);
             break;
           case 'channel.prediction.lock':
             processPredictionLock(data.payload.event);
